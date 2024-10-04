871415bf1b9668668a33d8565eac77b75178452c
Poweramo <assirouomar@gmail.com>
2024-10-04 20:19:34 +0100

    feat: display scores and add the ball with a random starting point

--- index.js
+++ index.js
@@ -1,38 +1,72 @@
-// TODO: add to github
-
 const canvasGame = document.getElementById("game-canvas");
 const canvasHeight = canvasGame.height;
 const canvasWidth = canvasGame.width;
 const context = canvasGame.getContext("2d");
 
-context.beginPath();
-context.strokeStyle = "white";
-context.lineWidth = 5;
-context.moveTo(canvasWidth / 2 , 0);
-context.lineTo(canvasWidth / 2, canvasWidth / 2);
-context.stroke();
+drawPlayer1();
+drawPlayer2();
+drawCentralLine();
+drawScores();
+generateBall();
 
-let x = 10;
-const playerHeight = 15;
-const y = (canvasHeight / 2);
+function drawCentralLine() {
+	context.beginPath();
+	context.strokeStyle = "white";
+	context.lineWidth = 5;
+	context.moveTo(canvasWidth / 2 , 0);
+	context.lineTo(canvasWidth / 2, canvasWidth / 2);
+	context.stroke();
+}
 
-context.beginPath();
-context.strokeStyle = "white";
-context.lineWidth = 5;
-context.moveTo(x, y);
-context.lineTo(x, y + playerHeight);
-context.moveTo(x, y);
-context.lineTo(x, y - playerHeight);
-context.stroke();
+function drawPlayer1() {
+	const x = 10;
+	const y = canvasHeight / 2;
+	const playerHeight = 15;
 
+	context.beginPath();
+	context.strokeStyle = "white";
+	context.lineWidth = 5;
+	context.moveTo(x, y);
+	context.lineTo(x, y + playerHeight);
+	context.moveTo(x, y);
+	context.lineTo(x, y - playerHeight);
+	context.stroke();
+}
 
-x = canvasWidth - x
+function drawPlayer2() {
+	const x = canvasWidth - 10;
+	const y = canvasHeight / 2;
+	const playerHeight = 15;
 
-context.beginPath();
-context.strokeStyle = "white";
-context.lineWidth = 5;
-context.moveTo(x, y);
-context.lineTo(x, y + playerHeight);
-context.moveTo(x, y);
-context.lineTo(x, y - playerHeight);
-context.stroke();
+	context.beginPath();
+	context.strokeStyle = "white";
+	context.lineWidth = 5;
+	context.moveTo(x, y);
+	context.lineTo(x, y + playerHeight);
+	context.moveTo(x, y);
+	context.lineTo(x, y - playerHeight);
+	context.stroke();
+}
+
+function drawScores() {
+	context.font = "15px Arial";
+	context.fillStyle = "white";
+	context.fillText("0", canvasWidth / 2 - 20, 20);
+	context.fillText("0", canvasWidth / 2 + 10, 20);
+}
+
+function generateBall() {
+	const x = canvasGame.width / 2;
+	const y = Math.floor(Math.random() * (canvasGame.height + 1));
+	const radius = 5;
+
+	context.beginPath();
+	context.strokeStyle = "green";
+	context.lineWidth = 2;
+	context.arc(x, y, radius, 0, 2 * Math.PI);
+	context.stroke();
+}
+
+function setBallFirstLineMvt() {
+	// code here
+}
