07d6ba89a434d34c2efaf23d968cadf2e7c885ca
Poweramo <assirouomar@gmail.com>
2024-10-13 17:39:36 +0100

    refactor: finished refactoring the code using a class && feat: make the yellow direction line invisible

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-// TODO: add moveBall method to the class and use the class methods to make the game work 
+// TODO: detect when the ball touches a player
 
 const canvasGame = document.getElementById("game-canvas");
 const canvasHeight = canvasGame.height;
 const canvasWidth = canvasGame.width;
 const context = canvasGame.getContext("2d");
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 class PingPong {
 	constructor() {
-		this.startY =  Math.floor(Math.random() * (canvasHeight + 1));
-		this.finishY = Math.floor(Math.random() * (canvasHeight + 1));
-		this.startX = Math.floor(canvasWidth / 2);
-		this.finishX = (this.finishY === 0 || this.finishY === canvasHeight) ? Math.floor(Math.random() * (this.startX + 1)) : 0;
+		this.ballCurrentY =  Math.floor(Math.random() * (canvasHeight + 1));
+		this.ballDestinationY = Math.floor(Math.random() * (canvasHeight + 1));
+		this.ballCurrentX = Math.floor(canvasWidth / 2);
+		this.ballDestinationX = (this.ballDestinationY === 0 || this.ballDestinationY === canvasHeight) ? Math.floor(Math.random() * (this.ballCurrentX + 1)) : 0;
 		this.playerHeight = 15;
 		this.firstPlayerX = 10;
 		this.firstPlayerY = canvasHeight / 2;
@@ -37,7 +38,7 @@ class PingPong {
 		context.fillText(this.scoreB, canvasWidth / 2 + 10, 20);
 	}
 	
-	drawPlayer() {
+	drawPlayer(x, y) {
 		context.beginPath();
 		context.strokeStyle = "white";
 		context.lineWidth = 5;
@@ -48,51 +49,69 @@ class PingPong {
 		context.stroke();
 	}
 		
-	generateBall(x, y) {
+	generateBall() {
 		const radius = 5;
 
 		context.beginPath();
 		context.strokeStyle = "green";
 		context.lineWidth = 2;
-		context.arc(x, y, radius, 0, 2 * Math.PI);
+		context.arc(this.ballCurrentX, this.ballCurrentY, radius, 0, 2 * Math.PI);
 		context.stroke();
+	}
+
+	drawEverything() {
+		context.clearRect(0, 0, canvasWidth, canvasHeight);
+		this.drawCentralLine();
+		this.drawScores();
+		this.drawPlayer(this.firstPlayerX, this.firstPlayerY);
+		this.drawPlayer(this.secondPlayerX, this.secondPlayerY);
+		this.generateBall();
+	}
+
+	async moveBall() {
+		while (this.ballCurrentX !== this.ballDestinationX || this.ballCurrentY !== this.ballDestinationY) {
+			const stepY = (this.ballDestinationY - this.ballCurrentY) / (this.ballDestinationX - this.ballCurrentX);
+
+			this.drawEverything();
+			if (this.ballCurrentX !== this.ballDestinationX) this.ballCurrentX > this.ballDestinationX ? this.ballCurrentX-- : this.ballCurrentX++;
+			if (Math.floor(this.ballCurrentY) !== this.ballDestinationY && Math.ceil(this.ballCurrentY) !== this.ballDestinationY) this.ballCurrentY -= stepY;
+			await delay(50)
+		}
+	}
+
+	movePlayer(player, sign) {
+		if (player === 1) {
+			sign === "+" ? this.firstPlayerY++ : this.firstPlayerY--;
+		} else {
+			sign === "+" ? this.secondPlayerY++ : this.secondPlayerY--;
+		}
+
+		this.drawEverything();	
 	}
 
 }
 
 const game = new PingPong();
 
-async function moveBall() {
-	let circleX = Math.floor(canvasGame.width / 2);
-	let circleY = Math.floor(Math.random() * (canvasGame.height + 1));
-	const mvtLineStartX = circleX;
-	const mvtLineStartY = circleY;
-	const mvtLineFinishY = Math.floor(Math.random() * (canvasGame.height + 1)); 
-	const delay = ms => new Promise(res => setTimeout(res, ms));
-	let y = canvasHeight / 2;
-
-
-	window.addEventListener("keydown", (e) => {
-		switch (e.key) {
-			case "ArrowUp":
-				if (y >= 15) drawEverything(circleX, circleY, mvtLineStartX, mvtLineStartY, mvtLineFinishX, mvtLineFinishY, y--);
-				break;
-			case "ArrowDown":
-				if (y <= canvasGame.height - 15) drawEverything(circleX, circleY, mvtLineStartX, mvtLineStartY, mvtLineFinishX, mvtLineFinishY, y++);
-				break;
-			default:
-				break;
-
-		}
-	})
-
-	while (circleX !== mvtLineFinishX || circleY !== mvtLineFinishY) {
-		const stepY = (mvtLineFinishY - mvtLineStartY) / (mvtLineFinishX - mvtLineStartX);
-
-		context.clearRect(0, 0, canvasWidth, canvasHeight);
-		drawEverything(circleX, circleY, mvtLineStartX, mvtLineStartY, mvtLineFinishX, mvtLineFinishY, y);
-		if (circleX !== mvtLineFinishX) circleX > mvtLineFinishX ? circleX-- : circleX++;
-		if (Math.floor(circleY) !== mvtLineFinishY && Math.ceil(circleY) !== mvtLineFinishY) circleY -= stepY;
-		await delay(50)
+game.drawCentralLine();
+game.drawScores();
+game.drawPlayer(game.firstPlayerX, game.firstPlayerY);
+game.drawPlayer(game.secondPlayerX, game.secondPlayerY);
+game.generateBall();
+game.moveBall();
+window.addEventListener("keydown", (e) => {
+	switch (e.key) {
+		case "ArrowUp":
+			if (game.firstPlayerY >= game.playerHeight) {
+				game.ballCurrentX <= canvasWidth / 2 ? game.movePlayer(1, "-") : game.movePlayer(2, "-");
+			}
+			break;
+		case "ArrowDown":
+			if (game.firstPlayerY <= canvasHeight - game.playerHeight) {
+				game.ballCurrentX <= canvasWidth / 2 ? game.movePlayer(1, "+") : game.movePlayer(2, "+");
+			}
+			break;
+		default:
+			break;
 	}
-}
+})
