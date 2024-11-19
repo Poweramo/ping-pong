3d33cd8999493e252668e9ccfdec2bb21a9d9dd2
Poweramo <assirouomar@gmail.com>
2024-11-19 12:44:23 +0100

    feat: return wich player the ball touched

--- utils/didBallTouchPlayer.js
+++ utils/didBallTouchPlayer.js
@@ -1,8 +1,12 @@
 export default function didBallTouchPlayer(player) {
         const distanceX = 5 + ballRadius;
-        if (player === 1) {
-                if (Math.round(ballCurrentX - firstPlayerX) === distanceX && Math.abs(Math.round(ballCurrentY - firstPlayerY)) <= playerHeight) return true;
-        } else {
-                if (Math.round(ballCurrentX - secondPlayerX) === -distanceX && Math.abs(Math.round(ballCurrentY - secondPlayerY)) > playerHeight) return true;
-        }
+
+        if (Math.round(ballCurrentX - firstPlayerX) === distanceX && Math.abs(Math.round(ballCurrentY - firstPlayerY)) <= playerHeight) {
+		ballDirection = ballDirection = "right" ? "left" : "right"; 
+		return 1;
+	}
+        if (Math.round(ballCurrentX - secondPlayerX) === -distanceX && Math.abs(Math.round(ballCurrentY - secondPlayerY)) > playerHeight) {
+		ballDirection = ballDirection = "right" ? "left" : "right"; 
+		return 2;
+	}
 }
