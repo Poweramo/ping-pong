140df3b204d15ad2db810ae9a7e0147d87299124
Poweramo <assirouomar@gmail.com>
2024-11-19 12:37:03 +0100

    refactor: change some functions names
    change generateTrajectory to generateStartingTrajectory and
    generateRandomTrajectory (split generateTrajectory into 2 functions)

--- utils/moveBall.js
+++ utils/moveBall.js
@@ -1,37 +1,30 @@
 import didBallTouchPlayer from "./didBallTouchPlayer.js";
-import generateTrajectory from "./generateTrajectory.js";
+import generateStartingTrajectory from "./generateStartingTrajectory.js";
+import generateRandomTrajectory from "./generateRandomTrajectory.js";
 import drawEverything from "./drawEverything.js";
 import changeScores from "./changeScores.js";
 import generateBall from "./generateBall.js";
+import didBallTouchSide from "./didBallTouchSide.js";
+import didBallTouchBorder from "./didBallTouchBorder.js";
 
 export default async function moveBall() {
         while (ballCurrentX !== ballDestinationX || (Math.floor(ballCurrentY) !== ballDestinationY && Math.ceil(ballCurrentY) !== ballDestinationY)) {
-                if (didBallTouchPlayer(1) || Math.floor(ballCurrentY) === 0 || Math.ceil(ballCurrentY) === 0 || Math.floor(ballCurrentY) === canvasHeight || Math.ceil(ballCurrentY) === canvasHeight) {
-                        generateTrajectory();
-                }
+                if (didBallTouchSide()) break;
+		if (didBallTouchPlayer() || didBallTouchSide()) generateRandomTrajectory(ballDirection);
 
-                const stepY = (ballDestinationY - ballCurrentY) / (ballDestinationX - ballCurrentX);
+                const stepY = Math.abs((ballDestinationY - ballCurrentY) / (ballDestinationX - ballCurrentX));
                 drawEverything();
-
-		console.log(stepY, ballCurrentY, ballDestinationY)
 
                 if (ballCurrentX !== ballDestinationX) ballCurrentX > ballDestinationX ? ballCurrentX-- : ballCurrentX++;
                 if (Math.floor(ballCurrentY) !== ballDestinationY && Math.ceil(ballCurrentY) !== ballDestinationY) {
-			if (ballCurrentY > ballDestinationY) {
-				stepY >= 0 ? ballCurrentY -= stepY : ballCurrentY += stepY;
-			} else {
-				stepY >= 0 ? ballCurrentY += stepY : ballCurrentY -= stepY;
-			}
+			ballCurrentY > ballDestinationY ? ballCurrentY -= stepY : ballCurrentY += stepY;
 		}
 
                 await delay(50);
         }
 
         changeScores();
-        ballCurrentY =  Math.floor(Math.random() * (canvasHeight + 1));
-        ballDestinationY = Math.floor(Math.random() * (canvasHeight + 1));
-        ballCurrentX = Math.floor(canvasWidth / 2);
-        ballDestinationX = (ballDestinationY === 0 || ballDestinationY === canvasHeight) ? Math.floor(Math.random() * (ballCurrentX + 1)) : 0;
+	generateStartingTrajectory(ballDirection);
         generateBall();
         moveBall();
 }
