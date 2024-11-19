de2b5473feb01e9d85d42db4b72fc2c88ec006f4
Poweramo <assirouomar@gmail.com>
2024-11-19 12:40:15 +0100

    refactor: delete this function

--- utils/generateTrajectory.js
+++
@@ -1,6 +0,0 @@
-export default function generateTrajectory() {
-	const minAddedValue = 20;
-
-        ballDestinationX = Math.round((Math.random() * (canvasWidth - ballCurrentX)) + ballCurrentX) + minAddedValue;
-        ballDestinationY = ballDestinationY === 0 ? canvasHeight : 0;
-}
