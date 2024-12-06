a36ad59087195535407c50ed689d9d528f600f31
Poweramo <assirouomar@gmail.com>
2024-12-06 15:07:14 +0100

    refactor: use ball direction to change scores
    use ball direction do know wich score to change rather than side number

--- utils/changeScores.js
+++ utils/changeScores.js
@@ -2,8 +2,6 @@ import drawEverything from "./drawEverything.js";
 import didBallTouchSide from "./didBallTouchSide.js";
 
 export default function changeScores() {
-	const side = didBallTouchSide();
-
-        side === 1 ? scoreB++ : scoreA++;
+        ballDirection === "right" ? scoreB++ : scoreA++;
         drawEverything();
 }
