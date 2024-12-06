3f25c1b1420ea7c409d88730bc9c4d467c586a0a
Poweramo <assirouomar@gmail.com>
2024-12-06 15:17:25 +0100

    fix: the ball on opposite direction at start
    change "left to "right"

--- utils/generateStartingTrajectory.js
+++ utils/generateStartingTrajectory.js
@@ -3,8 +3,8 @@ export default function generateStartingTrajectory(direction) {
         ballCurrentY =  Math.floor(Math.random() * canvasHeight);
 	ballDestinationY = Math.floor(Math.random() * canvasHeight);
 
-	if (direction === "left") {
-		ballDestinationX = (ballDestinationY === 0 || ballDestinationY === canvasHeight) ? Math.floor(Math.random() * (canvasWidth / 2)) : 0
+	if (direction === "right") {
+		ballDestinationX = (ballDestinationY === 0 || ballDestinationY === canvasHeight) ? Math.floor(Math.random() * (canvasWidth / 2)) : canvasWidth;
 	} else {
 		ballDestinationX = (ballDestinationY === 0 || ballDestinationY === canvasHeight) ? Math.floor(Math.random() * (canvasWidth / 2) + canvasWidth / 2) : 0;
 	}
