056c1f3413d781748c606c4fc4c113b033bddb27
Poweramo <assirouomar@gmail.com>
2024-12-06 14:59:47 +0100

    fix: random trajectories aren't logical
    make new algorithm that make random trajectory generated depending on
    different situations

--- utils/generateRandomTrajectory.js
+++ utils/generateRandomTrajectory.js
@@ -1,18 +1,18 @@
 export default function generateRandomTrajectory(direction) {
 	const distanceY = ballRadius + 5;
 	
-	if (direction === "right") {
-		ballDestinationX = Math.floor(Math.random() * ballCurrentX);
+	ballDestinationY = Math.floor(Math.random() * canvasHeight);
+
+	if (Math.floor(ballDestinationY) === 0 || Math.floor(ballDestinationY) === canvasHeight) {
+
+		if (direction === "left") {
+			ballDestinationX = Math.floor(Math.random() * ballCurrentX);
+		} else {
+			ballDestinationX = Math.floor(Math.random() * (canvasWidth - ballCurrentX) + ballCurrentX);
+		}
 
 	} else {
-		ballDestinationX = Math.floor(Math.random() * (canvasWidth - ballCurrentX) + ballCurrentX);
-	}
-
-
-	if (ballDestinationX === 0 || ballDestinationX === canvasWidth) {
-		ballDestinationY = Math.floor(Math.random() * (canvasHeight - ballCurrentY) + ballCurrentY);
-	} else {
-		ballDestinationY === 0 ? canvasWidth : 0;
+		ballDestinationX = Math.floor(ballDestinationX) === 0 ? canvasWidth : 0;
 	}
 
 }
