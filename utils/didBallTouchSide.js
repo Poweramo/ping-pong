66723502780db511df1614f603a6b115ae2ab961
Poweramo <assirouomar@gmail.com>
2024-12-06 15:13:47 +0100

    fix: ball direction not changing
    change "=" to "===" because it's a condition check not a variable change

--- utils/didBallTouchSide.js
+++ utils/didBallTouchSide.js
@@ -2,11 +2,11 @@ export default function didBallTouchSide() {
 	const distanceX = ballRadius + 5;
 
         if (Math.round(ballCurrentX) === distanceX) {
-		ballDirection = ballDirection = "right" ? "left" : "right"; 
-		return 1;
+		ballDirection = ballDirection === "right" ? "left" : "right";
+		return true;
 	}
         if (Math.round(ballCurrentX) === (canvasWidth - distanceX)) {
-		ballDirection = ballDirection = "right" ? "left" : "right"; 
-		return 2;
+		ballDirection = ballDirection === "right" ? "left" : "right";
+		return true;
 	}
 }
