d57a79e610459a938daf941b29c7ab1bbe830453
Poweramo <assirouomar@gmail.com>
2024-12-06 15:12:02 +0100

    feat: add changing direction
    add changing direction when the ball touches a player

--- utils/didBallTouchPlayer.js
+++ utils/didBallTouchPlayer.js
@@ -2,11 +2,12 @@ export default function didBallTouchPlayer(player) {
         const distanceX = 5 + ballRadius;
 
         if (Math.round(ballCurrentX - firstPlayerX) === distanceX && Math.abs(Math.round(ballCurrentY - firstPlayerY)) <= playerHeight) {
-		ballDirection = ballDirection = "right" ? "left" : "right"; 
-		return 1;
+		ballDirection = (ballDirection === "right" ? "left" : "right");
+		return true;
 	}
-        if (Math.round(ballCurrentX - secondPlayerX) === -distanceX && Math.abs(Math.round(ballCurrentY - secondPlayerY)) > playerHeight) {
-		ballDirection = ballDirection = "right" ? "left" : "right"; 
-		return 2;
+
+        if (Math.round(ballCurrentX - secondPlayerX) === -distanceX && Math.abs(Math.round(ballCurrentY - secondPlayerY)) <= playerHeight) {
+		ballDirection = (ballDirection === "right" ? "left" : "right");
+		return true;
 	}
 }
