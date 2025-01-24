409d12e9cc76060dd07b5c9c42702516424e6945
Poweramo <assirouomar@gmail.com>
2025-01-24 11:16:11 +0100

    fix: unconsistent bouncing/not bouncing on borders

--- utils/moveBall.js
+++ utils/moveBall.js
@@ -1,32 +1,35 @@
-import didBallTouchPlayer from "./didBallTouchPlayer.js";
-import generateStartingTrajectory from "./generateStartingTrajectory.js";
-import generateRandomTrajectoryForPlayers from "./generateRandomTrajectoryForPlayers.js";
-import generateRandomTrajectoryForBorders from "./generateRandomTrajectoryForBorders.js";
 import drawEverything from "./drawEverything.js";
-import changeScores from "./changeScores.js";
-import generateBall from "./generateBall.js";
 import didBallTouchSide from "./didBallTouchSide.js";
 import didBallTouchBorder from "./didBallTouchBorder.js";
+import changeScores from "./changeScores.js";
+import generateStartingTrajectory from "./generateStartingTrajectory.js";
+import didBallTouchPlayer from "./didBallTouchPlayer.js";
+import generateRandomTrajectory from "./generateRandomTrajectory.js";
+import generateBall from "./generateBall.js";
 
 export default async function moveBall() {
-        while (ballCurrentX !== ballDestinationX || (Math.floor(ballCurrentY) !== ballDestinationY && Math.ceil(ballCurrentY) !== ballDestinationY)) {
-                if (didBallTouchSide()) break;
-		if (didBallTouchPlayer()) generateRandomTrajectoryForPlayers(ballDirection);
-		if (didBallTouchBorder()) generateRandomTrajectoryForBorders(ballDirection);
+	let state = "border";
+	while (ballCurrentX !== ballDestinationX && Math.round(ballCurrentY) !== ballDestinationY) {
+		let stepY = Math.abs((ballDestinationY - ballCurrentY) / (ballDestinationX - ballCurrentX));
+        	drawEverything();
 
-                const stepY = Math.abs((ballDestinationY - ballCurrentY) / ((ballDestinationX - ballCurrentX) + speedFactor));
-                drawEverything();
-
-                if (ballCurrentX !== ballDestinationX) ballCurrentX > ballDestinationX ? ballCurrentX -= speedFactor : ballCurrentX += speedFactor;
-                if (Math.floor(ballCurrentY) !== ballDestinationY && Math.ceil(ballCurrentY) !== ballDestinationY) {
+        	if (ballCurrentX !== ballDestinationX) ballCurrentX > ballDestinationX ? ballCurrentX -= speedFactor : ballCurrentX += speedFactor;
+        	if (Math.round(ballCurrentY) !== ballDestinationY) {
 			ballCurrentY > ballDestinationY ? ballCurrentY -= stepY : ballCurrentY += stepY;
 		}
 
-                await delay(50);
-        }
+		if (didBallTouchSide()) {
+			changeScores();
+			generateBall();
+			state = "side";
+			break;
+		}
+		if (didBallTouchPlayer()) break;
 
-        changeScores();
-	generateStartingTrajectory(ballDirection);
-        generateBall();
-        moveBall();
+
+		await delay(20);
+	}
+
+	state === "side" ? generateStartingTrajectory(ballDirection) : generateRandomTrajectory(ballDirection);
+	moveBall();
 }
