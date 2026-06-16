cc4d43cf0f5e5c30c9fd67d5250de36a7915c94d
Poweramo <assirouomar@gmail.com>
2026-06-16 15:12:52 +0100

    fix: ball going beyond border limit

--- utils/didBallTouchBorder.js
+++ utils/didBallTouchBorder.js
@@ -1,6 +1,7 @@
 export default function didBallTouchBorder() {
 	const distanceY = ballRadius;
 
-        if (Math.round(ballCurrentY) === distanceY) return true;
-        if (Math.round(ballCurrentY) === (canvasHeight - distanceY)) return true;
+        if (ballCurrentY <= distanceY) return true;
+        if (ballCurrentY >= (canvasHeight - distanceY)) return true;
+	return false;
 }
