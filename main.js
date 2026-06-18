2e4578484dee2390f1033a726dab5a34374eaaa6
Poweramo <assirouomar@gmail.com>
2026-06-18 11:57:35 +0100

    refactor: move some global variables to func param

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-// Fix: ball touching the border and not bouncing (instead it counts as a goal)
-// Fix: ball bouncing on a border too fast and it counts as a goal when the ball touches the other border
-
+// FIX: ball is blinking
+// FIX: ball can get stuck in top middle or bottom middle of the table
 window.canvasGame = document.getElementById("game-canvas");
 window.canvasHeight = canvasGame.height;
 window.canvasWidth = canvasGame.width;
@@ -11,10 +10,10 @@ window.playerHeight = 15;
 window.firstPlayerX = 10;
 window.secondPlayerX = canvasWidth - firstPlayerX;
 window.ballDirection = "left";
-window.ballCurrentY =  Math.floor(Math.random() * (canvasHeight + 1));
+window.ballInitialY =  Math.floor(Math.random() * (canvasHeight + 1));
 window.ballDestinationY = Math.floor(Math.random() * (canvasHeight + 1));
-window.ballCurrentX = Math.floor(canvasWidth / 2);
-window.ballDestinationX = (ballDestinationY === 0 || ballDestinationY === canvasHeight) ? Math.floor(Math.random() * (ballCurrentX + 1)) : 0;
+window.ballInitialX = Math.floor(canvasWidth / 2);
+window.ballDestinationX = (ballDestinationY === 0 || ballDestinationY === canvasHeight) ? Math.floor(Math.random() * (ballInitialX + 1)) : 0;
 window.firstPlayerY = canvasHeight / 2;
 window.secondPlayerY = canvasHeight / 2;
 window.scoreA = 0;
@@ -26,35 +25,11 @@ import drawScores from "./utils/drawScores.js";
 import drawPlayer from "./utils/drawPlayer.js";
 import drawBall from "./utils/drawBall.js";
 import moveBall from "./utils/moveBall.js";
-import movePlayer from "./utils/movePlayer.js";
 
 drawCentralLine();
 drawScores();
 drawPlayer(firstPlayerX, firstPlayerY);
 drawPlayer(secondPlayerX, secondPlayerY);
-drawBall();
-moveBall(true, true);
-window.addEventListener("keydown", (e) => {
-	switch (e.key) {
-		case "ArrowUp":
-			if (firstPlayerY >= playerHeight) {
-				if (ballCurrentX <= canvasWidth / 2) movePlayer(1, "-");
-			}
-
-			if (secondPlayerY >= playerHeight) {
-				if (ballCurrentX > canvasWidth / 2) movePlayer(2, "-");
-			}
-			break;
-		case "ArrowDown":
-			if (firstPlayerY <= canvasHeight - playerHeight) {
-				if (ballCurrentX <= canvasWidth / 2) movePlayer(1, "+");
-			}
-
-			if (secondPlayerY <= canvasHeight - playerHeight) {
-				if (ballCurrentX > canvasWidth / 2) movePlayer(2, "+");
-			}
-			break;
-		default:
-			break;
-	}
-})
+drawBall(ballInitialX, ballInitialY);
+let gameStartTimeInSeconds = Temporal.Now.instant().epochMilliseconds * (10 ** -3);
+await moveBall(ballInitialX, ballInitialY, true, true, gameStartTimeInSeconds, gameStartTimeInSeconds);
