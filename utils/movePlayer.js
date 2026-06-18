e5aefc7e9299d11d1d5231c3951bf4edafc949f3
Poweramo <assirouomar@gmail.com>
2026-06-18 11:58:57 +0100

    refactor: move some global variables to func param

--- utils/movePlayer.js
+++ utils/movePlayer.js
@@ -1,11 +1,11 @@
 import drawEverything from "./drawEverything.js";
 
-export default function movePlayer(player, sign) {
+export default function movePlayer(player, sign, ballX, ballY) {
         if (player === 1) {
                 sign === "+" ? firstPlayerY += speedFactor: firstPlayerY -= speedFactor;
         } else {
                 sign === "+" ? secondPlayerY += speedFactor : secondPlayerY -= speedFactor;
         }
 
-        drawEverything();
+        drawEverything(ballX, ballY);
 }
